524dfc242f42908e2b43d4024f7cbfee9f653c49
Steven Lariau <obs145628@gmail.com>
2021-02-28 09:07:20 +0100

    Edit script to write binary data

--- barray.py
+++ barray.py
@@ -11,18 +11,28 @@ class BArrayOs:
     def write_to_file(self, path):
         with open(path, 'wb') as f:
             self._write_all(f)
-        
-    def _write_shape(self, os, shape):
-        data = array('i', [int(x) for x in shape])
+
+    def _write_int_array(self, os, data):
+        data = array('i', [int(x) for x in data])
+        data.tofile(os)
+
+    def _write_f32_array(self, os, data):
+        data = array('f', [float(x) for x in data])
         data.tofile(os)
 
     def _write_tensor(self, os, val):
-        self._write_shape(os, val.shape)
-        data = array('f', [float(x) for x in val.ravel()])
-        data.tofile(os)
+        # Write shape
+        self._write_int_array(os, [len(val.shape)])
+        self._write_int_array(os, val.shape)
+
+        # Write data
+        self._write_f32_array(os, val.ravel())
 
     def _write_all(self, os):
-        self._write_shape(os, [len(self._tensors)])
+        # Write number of tensors
+        self._write_int_array(os, [len(self._tensors)])
+
+        # Write all tensors
         for t in self._tensors:
             self._write_tensor(os, t)
 
